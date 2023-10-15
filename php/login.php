cf538de10ab8a87c13dbea20d74ba0f182ae292f
Irene <ivinentmercadal@iesjoanramis.org>
2023-10-15 17:09:01 +0200

    Edit forms

--- php/login.php
+++ php/login.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html>
+<head>
+  <title>Resposta formulari PHP</title>
+</head>
 <body>
   Welcome <?php echo $_GET["user-name"]; ?><br>
   Your email address is: <?php echo $_GET["user-email"]; ?>
